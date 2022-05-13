887ba13eb7330de5ccf0e7d4bd778602b197da9c
goelabhishek694 <goelabhishek694@gmail.com>
2022-05-13 15:25:26 +0530

    buy and sell stock 4th variation

---
+++ DP/27.buyAndSellStocksTransactionFee-InfiniteTransactionAllowed.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+
+        int fee=scn.nextInt();
+
+        int ans=bNSfeeInfiniteTran(arr,fee);
+        System.out.println(ans);
+    }
+
+    public static int bNSfeeInfiniteTran(int[] arr, int fee){
+        int bsp=-arr[0];
+        int ssp=0;
+
+        for(int i=1;i<arr.length;i++){
+            int nbsp=Math.max(bsp,ssp-arr[i]);
+            int nssp=Math.max(ssp,bsp+arr[i]-fee);
+
+            bsp=nbsp;
+            ssp=nssp;
+        }
+
+        return ssp;
+    }
+
+
+
+}
