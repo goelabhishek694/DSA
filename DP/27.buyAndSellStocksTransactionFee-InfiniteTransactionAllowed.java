e4139463cf3594ea8e8fda5034d83569e65c6987
goelabhishek694 <goelabhishek694@gmail.com>
2022-05-14 16:00:57 +0530

    leetcode 714

--- DP/27.buyAndSellStocksTransactionFee-InfiniteTransactionAllowed.java
+++ DP/27.buyAndSellStocksTransactionFee-InfiniteTransactionAllowed.java
@@ -33,7 +33,21 @@ public class Main {
 
         return ssp;
     }
+}
 
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int obsp=-prices[0];
+        int ossp=0;
 
+        for(int i=1;i<prices.length;i++){
+            int nbsp=Math.max(obsp,ossp-prices[i]);
+            int nssp=Math.max(ossp,obsp+prices[i]-fee);
 
+            obsp=nbsp;
+            ossp=nssp;
+        }
+
+        return ossp;
+    }
 }
