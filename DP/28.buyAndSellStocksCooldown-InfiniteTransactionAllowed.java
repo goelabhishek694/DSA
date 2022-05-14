aed222a6f6116d15529bd8e4f23f1ef851f8a479
goelabhishek694 <goelabhishek694@gmail.com>
2022-05-14 15:50:57 +0530

    leetcode 309

--- DP/28.buyAndSellStocksCooldown-InfiniteTransactionAllowed.java
+++ DP/28.buyAndSellStocksCooldown-InfiniteTransactionAllowed.java
@@ -45,3 +45,29 @@ public class Main {
     }
 
 }
+
+// leetcode 309
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int obsp=-prices[0];
+        int ossp=0;
+        int ocsp=0;
+        
+        for(int i=1;i<prices.length;i++){
+            int nbsp=0;
+            int nssp=0;
+            int ncsp=0;
+            
+            nbsp=Math.max(obsp,ocsp-prices[i]);
+            nssp=Math.max(ossp,prices[i]+obsp);
+            ncsp=Math.max(ocsp,ossp);
+            
+            obsp=nbsp;
+            ossp=nssp;
+            ocsp=ncsp;
+        }
+        
+        return ossp;
+    }
+}
