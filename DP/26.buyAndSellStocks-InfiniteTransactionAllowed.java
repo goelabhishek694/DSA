73d305ca32ae20b65e0754ba5e0ab2cfb31ff406
goelabhishek694 <goelabhishek694@gmail.com>
2022-05-14 17:41:05 +0530

    leetcode 122

--- DP/26.buyAndSellStocks-InfiniteTransactionAllowed.java
+++ DP/26.buyAndSellStocks-InfiniteTransactionAllowed.java
@@ -37,3 +37,22 @@ public class Main {
     }
 
 }
+
+// leetcode 122
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int obsp=-prices[0];
+        int ossp=0;
+        
+        for(int i=1;i<prices.length;i++){
+            int nbsp=Math.max(obsp,ossp-prices[i]);
+            int nssp=Math.max(ossp,obsp+prices[i]);
+            
+            obsp=nbsp;
+            ossp=nssp;
+        }
+        
+        return ossp;
+    }
+}
