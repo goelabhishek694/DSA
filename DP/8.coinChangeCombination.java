9b39b2c89cabdfff472462435dd47362c67bc9f7
goelabhishek694 <goelabhishek694@gmail.com>
2022-05-15 22:12:26 +0530

    leetcode 518

--- DP/8.coinChangeCombination.java
+++ DP/8.coinChangeCombination.java
@@ -53,3 +53,23 @@ public class Main {
         return dp[tar];
     }
 }
+
+// leetcode 518
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[]dp=new int[amount+1];
+        dp[0]=1;
+
+        for(int i=0;i<coins.length;i++){
+            int coin=coins[i];
+            for(int tar=1;tar<=amount;tar++){
+                if(tar-coin>=0){
+                    dp[tar]+=dp[tar-coin];
+                }
+            }
+        }
+
+        return dp[amount];
+    }
+}
